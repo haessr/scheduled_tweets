2f19d02bf74982264ee48e22e21f76179d81b738
Haess Romani <hromaza@gmail.com>
2021-04-01 19:56:47 -0500

    Edit password

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
   get "/about", to: "about#index"
   # get "/about-us", to: "about#index", as: "about"
 
+  get "/password", to: "passwords#edit", as: :edit_password
+  patch "/password", to: "passwords#update"
+
   get "/sign_up", to: "registrations#new"
   post "/sign_up", to: "registrations#create"
-  delete "/logout", to: "sessions#destroy"
-
+  
   get "/sign_in", to: "sessions#new"
   post "/sign_in", to: "sessions#create"
-
+  
+  delete "/logout", to: "sessions#destroy"
 
   # get "/", to: "main#index"
   root to: "main#index"
