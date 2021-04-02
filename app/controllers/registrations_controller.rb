5949f67b8d375a56c3615c1e53526ae488357410
Haess Romani <hromaza@gmail.com>
2021-04-01 19:00:56 -0500

    Added Login

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,24 @@ class RegistrationsController < ApplicationController
     # @user --> instance variable, visible in the views
     @user = User.new
   end
+
+  def create
+    # render plain: "Thanks!"
+    # render plain: params[:user]
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Successfully created account"
+    else
+      render :new
+    end
+    
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
   get "/sign_up", to: "registrations#new"
   post "/sign_up", to: "registrations#create"
+  delete "/logout", to: "sessions#destroy"
+
+  get "/sign_in", to: "sessions#new"
+  post "/sign_in", to: "sessions#create"
+
 
   # get "/", to: "main#index"
   root to: "main#index"
